f27e3c2e02574a450f8de29f11d520222b3a5b7b
aruuke <aruuke1357@gmail.com>
2023-04-24 08:03:15 +0600

    sldk

--- src/main/java/com/example/targetheartratecalculatorapp/TargetHeart.java
+++ src/main/java/com/example/targetheartratecalculatorapp/TargetHeart.java
@@ -12,7 +12,7 @@ public class TargetHeart extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(TargetHeart.class.getResource("TargetHeart.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Target-Heart-Rate Calculator App");
         stage.setScene(scene);
         stage.show();
